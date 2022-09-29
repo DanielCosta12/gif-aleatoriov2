55426b871ed99d4a0e0e4ba7fab6b84e718bccb6
DanielCosta12 <danieldossantosdacosta@gmail.com>
2022-09-29 02:31:22 -0300

    add search

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-const share_gif = document.querySelector("#share_gif");
-async function getData() {
+const container = document.querySelector("#container");
+container.hidden = true;
+const imgGif = document.querySelector("#imgGif");
+const btn = document.querySelector("#btn");
+
+function getShareGif() {
+  const search = document.querySelector("#search").value;
+
+  return getData(search);
+}
+async function getData(search) {
   try {
     const url = "https://tenor.googleapis.com/v2/search?q=";
-    const search_term = "bom-dia";
+    const search_term = search;
     const clientkey = "danielcosta12";
     const apikey = "AIzaSyDZeBxnHRZXj4kRHcDuJW57y2vUsZxoHS4";
-    const lmt = 0;
+    const lmt = 50;
     const response = await fetch(
       url +
         search_term +
@@ -24,9 +33,10 @@ async function getData() {
 }
 
 function addData(data) {
+  container.hidden = false;
   const random = Math.round(Math.random() * data.results.length);
   const urlGif = data.results[random].media_formats.gif.url;
   console.log(urlGif);
-  return (share_gif.src = urlGif);
+  return (imgGif.src = urlGif);
 }
-getData();
+btn.addEventListener("click", () => getShareGif());
